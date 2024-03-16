7abd5f52e3f0657d58df7af91651535cd9d3e60c
ACHECH Firas <152326654+achechfiras@users.noreply.github.com>
2024-03-16 12:26:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My React du firas et eya 
+          My React du firas et eya rsi3
         </p>
         <a
           className="App-link"
